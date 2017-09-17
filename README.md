89dd23d1f787e519d31baa8dae8d653ffa36928e
moritzmhmk <moritzmhmk@gmail.com>
2017-09-17 16:37:07 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Buildroot Camera RPi
+
+## License
+
+### buildroot-submodule
+[buildroot-submodule](https://github.com/Openwide-Ingenierie/buildroot-submodule) is provided under the GPLv3 or later. The licence is provided in the LICENCE file. Note that this licence only covers the files provided by buildroot-submodule. It does not cover buildroot (which is GPLv2 or later) nor any software installed by buildroot (they have their own licences) nor your own code (which you are free to licence as you want).
+
+### everything else ("my own code")
+All code written by me is provided under the *MIT License*.
---
+++ common.mk
@@ -0,0 +1,92 @@
+# This file is part of buildroot-submodule.
+#
+#    buildroot-submodule is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    buildroot-submodule is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with buildroot-submodule.  If not, see <http://www.gnu.org/licenses/>. 2
+
+#location of the buildroot sources
+MAKEARGS := -C $(CURDIR)/buildroot 
+#location to store build files
+MAKEARGS += O=$(CURDIR)/$(PROJECT_NAME)/output
+# location to store extra config options and buildroot packages
+MAKEARGS += BR2_EXTERNAL_BUILDROOT_SUBMODULE_PATH=$(CURDIR)
+#transmit project name to be able to use it in kconfig
+MAKEARGS += PROJECT_NAME=$(PROJECT_NAME)
+# location of default defconfig
+DEFCONFIG_FILE=$(CURDIR)/$(PROJECT_NAME)/defconfig
+DEFCONFIG := BR2_DEFCONFIG=$(DEFCONFIG_FILE)
+ALT_DEFCONFIG := BR2_DEFCONFIG=$(CURDIR)/defconfig
+
+MAKEFLAGS += --no-print-directory
+
+#these targets change the config file
+config_change_targets:=menuconfig nconfig xconfig gconfig oldconfig \
+       	silentoldconfig randconfig allyesconfig allnoconfig randpackageconfig \
+       	allyespackageconfig allnopackageconfig
+
+special_target:=$(config_change_targets) Makefile defconfig savedefconfig %_defconfig
+
+all	:= $(filter-out $(special_target),$(MAKECMDGOALS))
+
+default:  
+	$(MAKE) $(MAKEARGS) $(DEFCONFIG) defconfig
+	$(MAKE) $(MAKEARGS) $(DEFCONFIG)
+
+
+.PHONY: $(special_target) $(all) 
+
+# update from current config and save it as defconfig
+defconfig:
+	$(MAKE) $(MAKEARGS) $(ALT_DEFCONFIG) $@
+	$(MAKE) $(MAKEARGS) $(DEFCONFIG) savedefconfig
+
+# update from defconfig and save it as current configuration
+savedefconfig:
+	$(MAKE) $(MAKEARGS) $(DEFCONFIG) defconfig
+	$(MAKE) $(MAKEARGS) $(ALT_DEFCONFIG) savedefconfig
+
+# generate from a defconfig then save as current configuration
+%_defconfig:
+	$(MAKE) $(MAKEARGS) $(DEFCONFIG) $@
+	$(MAKE) $(MAKEARGS) $(DEFCONFIG) savedefconfig
+	$(call UPDATE_DEFCONFIG)
+
+
+# update from current configuration, run the command, then save the result
+$(config_change_targets): $(DEFCONFIG_FILE)
+	$(MAKE) $(MAKEARGS) $(DEFCONFIG) defconfig $@
+	$(MAKE) $(MAKEARGS) $(DEFCONFIG) savedefconfig
+
+_all:
+	$(MAKE) $(MAKEARGS) $(DEFCONFIG) $(all)
+
+$(all): _all
+	@:
+
+%/: _all
+	@:
+
+Makefile:;
+
+$(DEFCONFIG_FILE):
+	$(call UPDATE_DEFCONFIG)
+
+define UPDATE_DEFCONFIG
+	echo 'BR2_DL_DIR="$$(BR2_EXTERNAL_BUILDROOT_SUBMODULE_PATH)/dl"' >> $(DEFCONFIG_FILE)
+	echo 'BR2_ROOTFS_OVERLAY="$$(BR2_EXTERNAL_BUILDROOT_SUBMODULE_PATH)/overlay"' >> $(DEFCONFIG_FILE)
+	echo 'BR2_PACKAGE_OVERRIDE_FILE="$$(BR2_EXTERNAL_BUILDROOT_SUBMODULE_PATH)/local.mk"' >> $(DEFCONFIG_FILE)
+	echo 'BR2_GLOBAL_PATCH_DIR="$$(BR2_EXTERNAL_BUILDROOT_SUBMODULE_PATH)/patch"' >> $(DEFCONFIG_FILE)
+	$(MAKE) $(MAKEARGS) $(DEFCONFIG) defconfig
+	$(MAKE) $(MAKEARGS) $(DEFCONFIG) savedefconfig
+endef
+
+
